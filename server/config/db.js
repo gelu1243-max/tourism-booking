2db0bb474c38dadcbdc502eeb2412d362d99bf26
gelu1243-max <gelu1243@gmail.com>
2026-04-25 19:35:41 +0300

    Prepare frontend for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -5,13 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    logging: false, // Optional: prevents SQL logs from cluttering your Render console
-    dialectOptions: {
-        ssl: {
-            require: true,               // This is required for Neon
-            rejectUnauthorized: false,    // This is required for Render free tier
-        }
-    }
+    
 });
 
 export default sequelize;
